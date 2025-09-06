d48c40c43fba842666985ba928508037c32fb3b8
Tom Sullivan <tsullivan79@gmail.com>
2025-09-05 22:29:33 -0500

    build: remove experimental.esmExternals; transpile Sanity pkgs

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: { domains: ['cdn.sanity.io'] },
-
-  // 👇 Helps Next/Vercel build Sanity packages cleanly
   transpilePackages: ['sanity', '@sanity/vision', 'next-sanity', '@sanity/ui'],
-  experimental: { esmExternals: 'loose' },
 }
-
 export default nextConfig
+
